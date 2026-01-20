0895cd2fdb45e8e4cd67c08c533e30ba2e88433b
pickleballattrespalapas <pickleballattrespalapas@gmail.com>
2026-01-20 14:26:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 # jupr/app.py
+from pathlib import Path
+import sys
+import streamlit as st
+
+THIS_DIR = Path(__file__).parent
+sys.path.insert(0, str(THIS_DIR))
+
+st.write("Booting JUPR…")
+
 import streamlit_app  # noqa: F401
+if hasattr(streamlit_app, "main"):
+    streamlit_app.main()
