ad617328dcbac8cb7805e85fdb78124fe9dc7de7
pickleballattrespalapas <pickleballattrespalapas@gmail.com>
2026-02-18 18:26:06 -0700

    Fix Supabase client options for current SDK

--- jupr_app/data/client.py
+++ jupr_app/data/client.py
@@ -1,12 +1,15 @@
-from supabase import Client, create_client
+from supabase import create_client, Client
+from supabase.lib.client_options import ClientOptions
 
 
 def make_supabase(url: str, key: str) -> Client:
+    options = ClientOptions(
+        persist_session=True,
+        auto_refresh_token=True,
+    )
+
     return create_client(
         url,
         key,
-        options={
-            "persist_session": True,
-            "auto_refresh_token": True,
-        },
+        options=options,
     )
